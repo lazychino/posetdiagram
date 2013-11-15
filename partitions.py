f1849eacc71edb4cffce1c0412e71e6f1ecad6e2
Pedro A. Melendez <lazy.chino@gmail.com>
2013-11-14 22:59:18 -0400

    lower memory transactions and use threads to better performance

--- partitions.py
+++ partitions.py
@@ -1,38 +1,47 @@
 from itertools import combinations, permutations
-import copy
+from threading import Thread
 import datetime
 
-def distintJoin(P, sets, join):
+rtn = [] # global list for use as a stack for the thread
+
+def distintJoin(P, P2, join):
+    ''' makes a disjoint join of partion '''
     for p in P:        
-        for st in sets:
-            temp = copy.deepcopy(p)
-            #~ print 'st', st
-            #~ print 'p ', p
-            if all(x.isdisjoint(y) for x in st for y in p):
-                #~ print '   disjoint'
-                temp = temp.union(st)
-                if all(not temp.issubset(p) for p in join):
-                    join.append(temp)
+        for p2 in P2:
+            if all(x.isdisjoint(y) for x in p2 for y in p):
+                tmp = p.union(p2)
+                if all(not tmp.issubset(i) for i in join):
+                    join.append(tmp)
     return join
+
+    
+def genCombinationOfSizeN(S, n):
+   rtn.append([frozenset([frozenset(x)]) for x in list(combinations(S,n))])
+
 
 def SetPartitions(S):
     s = len(S)
+    # elements of cardinality 1
     combi = list()
     combi.append([frozenset([frozenset([x])]) for x in S])
-
+    
+     # elements of cardinality 2
     combi.append([frozenset(x) for x in list(combinations([frozenset([x]) for x in S],2))])
     combi[1] += [frozenset([frozenset(x)]) for x in list(combinations(S,2))]
         
-    for i in range(2,s):    # combi[i] have element of cardinality i+1
-        temp = []
+    for i in range(2,s):    # combi[i] have elements of cardinality i+1
+        temp = list()
+        tred = Thread(target=genCombinationOfSizeN, args= (S, i+1))  # parallelization
+        tred.start()
         for k in range((i+1)/2):
             temp = distintJoin(combi[i-(k+1)], combi[k], temp)
-        temp += [frozenset([frozenset(x)]) for x in list(combinations(S,i+1))]
-        combi.append(copy.deepcopy(temp))
-        del temp
+        tred.join()
+        temp += rtn.pop()
+        combi.append(temp)
     
     return combi[s-1]
 
+#--------------testing code-------------------------------- 
 #~ n = input('enter n:')
 #~ 
 #~ t1 = datetime.datetime.today()
