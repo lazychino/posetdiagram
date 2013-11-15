eaa4feb4ad2a364a0c88aca77ee9db60ddfdc762
Pedro A. Melendez <lazy.chino@gmail.com>
2013-11-15 02:28:06 -0400

    completed generation of poset and conversion to JSON, a sampleposet.json is a file format sample for frontend dev

--- poset.py
+++ poset.py
@@ -1,28 +1,65 @@
-orderedpartitions = []
-for i in range(n+1):
-    orderedpartitions.append([])
+import partitions
 
-#~ print partitions
+def orderSet(a, b):
+    return len(a) - len(b)
 
-for p in partitions:
-    m = len(p)
-    p.sort()
-    p.reverse()
-    orderedpartitions[m].append(p)
+def partiToString(S):  
+    ''' convert partition to string representation '''
+    return "|".join(["".join(str(e) for e in sorted(s)) for s in S])
+    
+def getRelations(Parts, n):
+    ''' 
+    returns a dictionary with the list of children for each partition on format part_1|part_2|...|part_n
+    format example for set "abcd": a|b|cd
+    '''
+    
+    orderedpartitions = [list() for i in range(n+1)]
 
-orderedpartitions.pop(0)
-orderedpartitions.reverse()
+    for p in Parts:
+        p = list(p)
+        m = len(p)
+        p.sort(orderSet)
+        p.reverse()
+        orderedpartitions[m].append(p)
 
-#~ print orderedpartitions
-for i in orderedpartitions:
-    print i
+    orderedpartitions.pop(0)
+    orderedpartitions.reverse()
+
+    posetDict= dict()
+    for i in reversed(range(1,n)):
+        for x in orderedpartitions[i]:
+            posetDict[partiToString(x)] = []
+            if (i-1) == 0:
+                posetDict[partiToString(x)].append(partiToString(orderedpartitions[0][0]))
+            else:
+                for y in orderedpartitions[i-1]:
+                    if any(y[0].issubset(s) for s in x if len(s) > 1):
+                        posetDict[partiToString(x)].append(partiToString(y))
+    posetDict[partiToString(orderedpartitions[0][0])] = None
+    posetDict['root'] = partiToString(orderedpartitions[n-1][0])
+    return posetDict
+
+def toJSON(P, parent):
+    if P[parent] == None:
+        return '{ "name": "%s" }' % (parent)
+    else:
+        arr = []
+        for child in P[parent]:
+            arr.append(toJSON(P, child))
+        children = ",".join(arr)
+        return '{ "name": "%s", "children": [ %s ] }' % (parent, children)
+
+def getDiagramPosetOf(S):
+    ''' takes a antichain set and return the poset for all posible equivalent diagrams '''
+    parts = partitions.SetPartitions(S)
+    poset = getRelations(parts, len(S))
+    return toJSON(poset, poset['root'])
+ 
+#---------testing code ----------------------
+#~ SET = 'abcd'
+#~ f = open("poset.json", 'w')
+#~ f.write(getDiagramPosetOf(SET))
+#~ f.close()
 
 
-print "\n\nPOSET"
-for i in reversed(range(2,n)):
-    for x in orderedpartitions[i]:
-        print '\n', x, '->',
-        for y in orderedpartitions[i-1]:
-            if y[0].issubset(x[0]):
-                print y, ' ',
-        sys.stdout.flush()
+
