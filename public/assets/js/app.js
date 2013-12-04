49db3450eeef119d063e1e7c5793101572faaead
Pedro A. Melendez <lazy.chino@gmail.com>
2013-12-04 18:11:42 -0400

    added loading image

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 				var number = Number($('#ac_number').val());
 				if(isNaN(number)) 
 					return;
+				$('#poset').empty().append('<img class="loading" src="/assets/loading.gif" alt="loading">');
 				$.get("/antichain/"+number, function( data ) {
 					$('#poset').empty();
 					drawPoset(data, number);
@@ -17,12 +18,12 @@ $(document).ready(function() {
 		});
 	}).click();
 	
-	$('#team-link').on('click', function() {		
+	$('#team-link').on('click', function() {
 		$.get("/team.html", function( data ) {
 			$('#view').empty().append(data);
 		});
 	});
-	$('#about-link').on('click', function() {		
+	$('#about-link').on('click', function() {
 		$.get("/about.html", function( data ) {
 			$('#view').empty().append(data);
 		});
