b30da2cbeaba5258869d84572e5b2fd4c47c57c6
Pedro A. Melendez <lazy.chino@gmail.com>
2013-12-04 18:34:10 -0400

    limit max poset

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,6 +9,10 @@ $(document).ready(function() {
 				var number = Number($('#ac_number').val());
 				if(isNaN(number)) 
 					return;
+				if(number > 6) {
+					$('#poset').empty().append("sorry for limit with memory on host this can not be generated<br>\nto run n&ge;7 download from repository and run locally on your pc");
+					return;
+				}
 				$('#poset').empty().append('<img class="loading" src="/assets/loading.gif" alt="loading">');
 				$.get("/antichain/"+number, function( data ) {
 					$('#poset').empty();
