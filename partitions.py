eef8234808788ad300650939ddf936ac8c48077c
Pedro A. Melendez <lazy.chino@gmail.com>
2013-12-05 03:30:39 -0400

    update test code and documentation

--- partitions.py
+++ partitions.py
@@ -1,6 +1,6 @@
 from itertools import combinations, permutations
 from threading import Thread
-import datetime
+
 
 rtn = [] # global list for use as a stack for the thread
 
@@ -14,9 +14,10 @@ def distintJoin(P, P2, join):
                     join.append(tmp)
     return join
 
-    
+
 def genCombinationOfSizeN(S, n):
-   rtn.append([frozenset([frozenset(x)]) for x in list(combinations(S,n))])
+    ''' this generate subsets of size n fron the set S '''
+    rtn.append([frozenset([frozenset(x)]) for x in list(combinations(S,n))])
 
 
 def SetPartitions(S):
@@ -28,13 +29,14 @@ def SetPartitions(S):
      # generate elements of cardinality 2
     combi.append([frozenset(x) for x in list(combinations([frozenset([x]) for x in S],2))])
     combi[1] += [frozenset([frozenset(x)]) for x in list(combinations(S,2))]
-        
+    
     for i in range(2,s):    # combi[i] have elements of cardinality i+1
         temp = list()
         tred = Thread(target=genCombinationOfSizeN, args= (S, i+1))  # parallelization
         tred.start()
         for k in range((i+1)/2):
-            temp = distintJoin(combi[i-(k+1)], combi[k], temp)
+            #combine subsets so that the sum of their cardinalities is equal to i
+            temp = distintJoin(combi[i-(k+1)], combi[k], temp) 
         tred.join()
         temp += rtn.pop()
         combi.append(temp)
@@ -43,7 +45,8 @@ def SetPartitions(S):
 
 
 if __name__ == "__main__":
-#--------------testing code--------------------------------
+#--------------testing code-------------------------------- 
+    import datetime
     print "Set partitions genarator"
     s = input("enter set(list or string): ")
     print "Set =", s
--- poset.py
+++ poset.py
@@ -2,6 +2,7 @@ import partitions
 import json
 
 def orderSet(a, b):
+    ''' order element of partitions '''
     if( len(a) == 1 and len(b) == 1):
         return list(b)[0] - list(a)[0]
         
@@ -12,12 +13,15 @@ def partiToString(S):
     return "|".join(["".join(str(e) for e in sorted(s)) for s in S])
 
 def partToDict(p, level):
+    ''' create node object '''
     return {'name': partiToString(p), 'level': level }
 
 def getRelations(Parts, n):
     ''' 
-    returns a dictionary with the list of children for each partition on format part_1|part_2|...|part_n
-    format example for set "abcd": a|b|cd
+    returns a dictionary with the list of nodes and list of links, 
+    each node have the level on the poset and its string respresentation 
+    string format: part_1|part_2|...|part_n 
+    format example partititon {{a},{b},{c,d}} = "a|b|cd"
     '''
     
     orderedpartitions = [list() for i in range(n+1)]
@@ -40,20 +44,20 @@ def getRelations(Parts, n):
     for i in reversed(range(1,n)):
         for x in orderedpartitions[i]:
             partX = partToDict(x,i)
-            if partX not in nodes:
+            if partX not in nodes:   #check if partition is on graph nodes list
                 nodes.append(partX)
-            if (i-1) == 0:
+            if (i-1) == 0: #if next node is null link to it
                 links.append({ 'source': nodes.index(partX), 'target': nodes.index(partToDict(orderedpartitions[0][0], 0))})
             else:
                 for y in orderedpartitions[i-1]:
                     partY = partToDict(y,(i-1))
-                    if partY not in nodes:
+                    if partY not in nodes: 
                         nodes.append(partY)
-                    if any(y[0].issubset(s) for s in x if len(s) > 1):
+                    if any(y[0].issubset(s) for s in x if len(s) > 1): # if partitionY is subset of partitionX create a link between nodes
                         links.append({ 'source': nodes.index(partX), 'target': nodes.index(partY) })
     return {'nodes': nodes, 'links': links }
 
-def toJSONTree(P, parent):
+def toJSONTree(P, parent): #not used
     if P[parent] == None:
         return '{ "name": "%s" }' % (parent)
     else:
@@ -72,11 +76,14 @@ def getDiagramPosetOf(S):
 
 if __name__ == "__main__":
 #---------testing code ----------------------
+    import datetime
     print "Antichain poset of diagram genarator"
     antichain = input("enter antichain size:")
+    t1 = datetime.datetime.today()
     poset = getDiagramPosetOf(range(antichain))
-    print poset
+    t2 = datetime.datetime.today()
     f = open("poset.json", 'w')
     f.write(poset)
     print "the poset is on JSON format in the file 'poset.json'"
     f.close()
+    print 'time to generate', t2-t1
